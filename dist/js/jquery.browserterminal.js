aea9ae64d9a23f14c49256f8efa93440984e7ae0
Jörg Breitbart <jerch@rockborn.de>
2015-01-08 02:33:07 +0100

    keyhandler improved

--- dist/js/jquery.browserterminal.js
+++ dist/js/jquery.browserterminal.js
@@ -4,137 +4,125 @@
  */
 (function($) {
     'use strict';
-
-    var nonChar = false;
-
-    var functionkeys = {
-        1   : '\u001bOP',
-        2   : '\u001bOQ',
-        3   : '\u001bOR',
-        4   : '\u001bOS',
-        5   : '\u001b[15~',
-        6   : '\u001b[17~',
-        7   : '\u001b[18~',
-        8   : '\u001b[19~',
-        9   : '\u001b[20~',
-        10  : '\u001b[21~',
-        11  : '\u001b[23~',
-        12  : '\u001b[24~'
-    };
     
     function KeyHandler() {
-
-        this.handle_keys = function(that) {
-            return function(e) {
-                // dont handle any other elements than terminals
-                var term = $(document.activeElement).data('terminal');
-                if (!(term instanceof BrowserTerminal))
-                    return;
-
-//            term.widget.scrollTop(term.widget.prop('scrollHeight') - term.widget.innerHeight());
-                var char;
-                var evt = (e) ? e : window.event;       //IE reports window.event not arg
-                if (evt.type == "keydown") {
-                    var ret;
-                    char = evt.keyCode;
-                    if ((char && char < 16) ||                    // non printables -- also exclude char 0
-                        (char > 16 && char < 32) ||     // avoid shift
-                        (char > 32 && char < 41) ||     // navigation keys
-                        char == 46) {                   // Delete Key (Add to these if you need)
-                        ret = that.handleNonChar(evt, char, term);            // function to handle non Characters
-                        nonChar = true;
-                    } else
-                        nonChar = false;
-                } else {                                // This is keypress
-                    if (nonChar) return;                // Already Handled on keydown
-                    char = (evt.charCode) ?
-                        evt.charCode : evt.keyCode;
-                    if (char > 31)        // safari and opera
-                        ret = that.handleChar(evt, char, term);               //
-                }
-                if (e) {
-                    //        jQuery.Event(evt).stopPropagation();
-                    //        jQuery.Event(evt).preventDefault();
-                }                                // Non IE
-                else if (evt.keyCode == 8)              // Catch IE backspace
-                    evt.returnValue = false;            // and stop it!
-
-                // special treatment for chrome f keys
-                if ($.browser.chrome && evt.type == "keydown" && e.keyCode > 111 && e.keyCode < 124) {
-                    term.chars += functionkeys[e.keyCode-111];
-                    jQuery.Event(evt).preventDefault();
-                }
-                return ret;
-            }
+        // see http://unixpapa.com/js/key.html
+        var F_KEYS = {
+            1 : '\u001bOP',
+            2 : '\u001bOQ',
+            3 : '\u001bOR',
+            4 : '\u001bOS',
+            5 : '\u001b[15~',
+            6 : '\u001b[17~',
+            7 : '\u001b[18~',
+            8 : '\u001b[19~',
+            9 : '\u001b[20~',
+            10: '\u001b[21~',
+            11: '\u001b[23~',
+            12: '\u001b[24~'
         };
+        var nonChar = false;  // closure flag to distinct non chars for keypress
 
-        this.handleNonChar = function(e, c, term) {
-            switch (c) {
-                case 8:  term.chars += '\u0008'; break;
-                case 9:  term.chars += '\t'; break;
-                case 10: term.chars += '\n'; break;
-                case 13: term.chars += '\r'; break;
-                case 27: term.chars += '\u001b'; break;
-                case 37: term.chars += term.arrowkey('left'); break;
-                case 38: term.chars += term.arrowkey('up'); break;
-                case 39: term.chars += term.arrowkey('right'); break;
-                case 40: term.chars += term.arrowkey('down');  break;
-                default:
-                    console.debug('unhandled nonChar:', c);
-            }
-            //    jQuery.Event(e).stopPropagation();
-            //    jQuery.Event(e).preventDefault();
-            return false;
-        };
-
-        this.handleChar = function(e, c, term) {
-            // --> f-keys differ in key/charcode
-            if (e.keyCode > 111 && e.keyCode < 124 && e.charCode == 0) {
-                // function key
-                term.chars += functionkeys[e.keyCode-111];
-                jQuery.Event(e).stopPropagation();
-                //        jQuery.Event(e).preventDefault();
+        this.handle_keys = function(evt) {
+            // handle only terminal elements
+            var term = $(document.activeElement).data('terminal');
+            if (!(term instanceof BrowserTerminal))
                 return;
+            //console.debug(evt.type, evt.keyCode, evt.charCode, evt.ctrlKey, evt.altKey);
+            var ret = true,
+                char;
+            if (evt.type == 'keydown') {
+                char = evt.keyCode;
+
+                // SHIFT, CTRL, ALT
+                if (char == 16 || char == 17 || char == 18) {
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                    return false;
+                }
+
+                // function keys
+                if (evt.keyCode > 111 && evt.keyCode < 124) {
+                    term.chars += F_KEYS[evt.keyCode - 111];
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                    return false;
+                }
+
+                // non chars on keydown
+                if ((char && char < 16) ||          // non printables -- also exclude char 0
+                    (char > 16 && char < 32) ||     // avoid shift
+                    (char > 32 && char < 41) ||     // navigation keys
+                    char == 46 || char == 45 ||     // delete and insert key
+                    (evt.ctrlKey && !evt.altKey))   // handle all CTRL+x keys with keydown
+                {
+                    if (evt.ctrlKey) {
+                        var chars = '@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_';
+                        var idx = chars.indexOf(String.fromCharCode(evt.keyCode));
+                        if (idx != -1)
+                            term.chars += String.fromCharCode(idx);
+                        else
+                            console.debug('unhandled nonChar:', c);
+                    } else {
+                        switch (char) {
+                            case 8:  term.chars += '\u0008'; break;
+                            case 9:  term.chars += '\t'; break;
+                            case 10: term.chars += '\n'; break;
+                            case 13: term.chars += '\r'; break;
+                            case 27: term.chars += '\u001b'; break;
+                            case 33: term.chars += '\u001b[5~'; break;  // page up
+                            case 34: term.chars += '\u001b[6~'; break;  // page down
+                            case 35: term.chars += term.ckm('end'); break;
+                            case 36: term.chars += term.ckm('home'); break;
+                            case 37: term.chars += term.ckm('left'); break;
+                            case 38: term.chars += term.ckm('up'); break;
+                            case 39: term.chars += term.ckm('right'); break;
+                            case 40: term.chars += term.ckm('down');  break;
+                            case 45: term.chars += '\u001b[2~'; break;  // insert
+                            case 46: term.chars += '\u001b[3~'; break;  // delete
+                            default:
+                                console.debug('unhandled nonChar:', c);
+                        }
+                    }
+                    evt.stopPropagation();
+                    evt.preventDefault();
+                    nonChar = true;
+                    return false;
+                } else {
+                    nonChar = false;
+                }
+            } else {                                // keypress
+                if (nonChar)
+                    return false;                   // non char handled on keydown
+                // normal chars
+                char = (evt.charCode) ? evt.charCode : evt.keyCode;
+                if (char > 31) {
+                    term.chars += String.fromCharCode(char);
+                    ret = false;
+                }
             }
-            var char = String.fromCharCode(c);
-            if (e.ctrlKey) {
-                // see http://en.wikipedia.org/wiki/C0_and_C1_control_codes
-                var chars = '@abcdefghijklmnopqrstuvwxyz[\]^_';
-                var idx = chars.indexOf(char);
-                if (idx != -1)
-                    term.chars += String.fromCharCode(idx);
-            } else
-                term.chars += char;
-            //    jQuery.Event(e).stopPropagation();
-            //    jQuery.Event(e).preventDefault();
-            return false;
+            return ret;
         };
 
         // from http://codebits.glennjones.net/editing/getclipboarddata.htm
         this.handle_paste = function(e) {
-            // dont handle any other elements than terminals
+            // handle only terminal elements
             var term = $(document.activeElement).data('terminal');
-            //if (!(term instanceof BrowserTerminal))
-            //    return;
+            if (!(term instanceof BrowserTerminal))
+                return;
             if (e.clipboardData) {
                 term.chars += e.clipboardData.getData('text/plain');
             } else if (window.clipboardData) {  // IE
                 term.chars += window.clipboardData.getData('Text');
             }
-            jQuery.Event(e).stopPropagation();
-            jQuery.Event(e).preventDefault();
+            e.stopPropagation();
+            e.preventDefault();
         };
 
         // attach global handler to DOM
-        document.onkeydown = this.handle_keys(this);
-        document.onkeypress = this.handle_keys(this);
+        document.onkeydown = this.handle_keys;
+        document.onkeypress = this.handle_keys;
         document.onpaste = this.handle_paste;
-    }
-    
-    function appendClone(node) {
-        return function(el) {
-            node.appendChild(el.cloneNode(true));
-        }
     }
     
     function BrowserTerminal(el, options) {
@@ -186,7 +174,7 @@
             return function(elems){
                 if (that.bufferLength) {
                     that.scrollBufferChanged = true;
-                    that.scroll_buffer.push(that.createPrintFragment([elems]));
+                    that.scroll_buffer.push([[elems], null]);
                     while (that.scroll_buffer.length >= that.bufferLength)
                         that.scroll_buffer.shift();
                 }
@@ -217,8 +205,8 @@
             // remove cursor from buffer
             if (this.terminal.show_cursor)
                 if (buffer[row][col]) {
-                    if (buffer[row][col].attributes)
-                        buffer[row][col].attributes[3] = null;
+                    if (buffer[row][col][1])
+                        buffer[row][col][1][3] = null;
                 }
             // apply all changes to terminal
             this.parser.parse(s);
@@ -228,15 +216,15 @@
             col = this.terminal.cursor.col;
             if (this.terminal.show_cursor)
                 if (buffer[row][col]) {
-                    if (!buffer[row][col].attributes)
-                        buffer[row][col].attributes = [null, null, null, null, null, null, null, null];
+                    if (!buffer[row][col][1])
+                        buffer[row][col][1] = [null, null, null, null, null, null, null, null];
                     else
-                        buffer[row][col].attributes = buffer[row][col].attributes.slice();
-                    buffer[row][col].attributes[3] = (this.terminal.blinking_cursor) ? ' blc c' : ' c';
+                        buffer[row][col][1] = buffer[row][col][1].slice();
+                    buffer[row][col][1][3] = (this.terminal.blinking_cursor) ? ' blc c' : ' c';
                 }
         };
 
-        this.arrowkey = function(s) {
+        this.ckm = function(s) {
             //var code = (this.terminal.cursor_key_mode) ? TERM_STRING['SS3'] : TERM_STRING['CSI'];
             var code = (this.terminal.cursor_key_mode) ? '\u001bO' : '\u001b[';
             switch (s) {
@@ -244,6 +232,8 @@
                 case 'up'    : code +='A'; break;
                 case 'right' : code +='C'; break;
                 case 'down'  : code +='B'; break;
+                case 'end'   : code +='F'; break;
+                case 'home'  : code +='H'; break;
             }
             return code;
         };
@@ -257,7 +247,7 @@
                 attr = null;
             for (var i=0; i<buffer.length; ++i) {
                 for (var j=0; j<buffer[i].length; ++j) {
-                    attr = buffer[i][j].attributes;
+                    attr = buffer[i][j][1];
                     if (old_attr !== attr) {
                         if (old_attr) {
                             span.textContent = s;
@@ -275,13 +265,13 @@
                             span.setAttributeNode(clas);
                             if (attr[3]) {
                                 var data_contents = document.createAttribute('data-contents');
-                                data_contents.value = buffer[i][j].c || '\xa0';
+                                data_contents.value = buffer[i][j][0] || '\xa0';
                                 span.setAttributeNode(data_contents);
                             }
                         }
                         old_attr = attr;
                     }
-                    s += buffer[i][j].c || '\xa0';
+                    s += buffer[i][j][0] || '\xa0';
                 }
                 if (old_attr) {
                     old_attr = null;
@@ -332,7 +322,12 @@
                         if (that.terminal.buffer == that.terminal.normal_buffer) {
                             if (that.scrollBufferChanged) {
                                 var new_el = that.el_scroll.cloneNode(false);
-                                that.scroll_buffer.map(appendClone(new_el));
+                                for (var i = 0; i<that.scroll_buffer.length; ++i) {
+                                    new_el.appendChild(
+                                        (that.scroll_buffer[i][1] ||
+                                            (that.scroll_buffer[i][1]=that.createPrintFragment(that.scroll_buffer[i][0]))
+                                        ).cloneNode(true));
+                                }
                                 that.el_scroll.parentNode.replaceChild(new_el, that.el_scroll);
                                 that.el_scroll = new_el;
                                 that.scrollBufferChanged = false;
@@ -354,8 +349,10 @@
 
                     // title
                     that.setTitle.call(that.el, that.terminal.title);
-                    // scroll down
-                    that.container[0].scrollTop = that.container[0].scrollHeight;
+                    // scroll down - lazy to avoid early reflow
+                    setTimeout(function() {
+                        that.container[0].scrollTop = that.container[0].scrollHeight;
+                    }, 0);
                 }
                 
                 // continuation
